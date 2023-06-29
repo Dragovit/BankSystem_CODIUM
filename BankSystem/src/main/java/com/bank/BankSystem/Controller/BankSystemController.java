c04e558c9b1d7423c6bd2b32f6c519f97d1a0e70
Dragovit <palitomcik@gmail.com>
2023-06-29 17:03:00 +0200

    HTTP code status handled

--- BankSystem/src/main/java/com/bank/BankSystem/Controller/BankSystemController.java
+++ BankSystem/src/main/java/com/bank/BankSystem/Controller/BankSystemController.java
@@ -8,6 +8,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -34,7 +36,7 @@ public class BankSystemController {
         } if(person.getPersonalNumber() == null) {
             error = error + "personalNumber";
         }
-        if (error.equals("")) {
+        if (!error.equals("")) {
             return ResponseEntity.badRequest().body("Invalid input missing: " + error);
         }
         else {
