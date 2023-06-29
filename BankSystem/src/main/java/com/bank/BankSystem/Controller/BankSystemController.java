7c190a6ec71943545dacf98f9a709fe97c160e4e
Dragovit <palitomcik@gmail.com>
2023-06-29 15:05:24 +0200

    Profiles was added and logs

--- BankSystem/src/main/java/com/bank/BankSystem/Controller/BankSystemController.java
+++ BankSystem/src/main/java/com/bank/BankSystem/Controller/BankSystemController.java
@@ -3,7 +3,11 @@ package com.bank.BankSystem.Controller;
 import com.bank.BankSystem.Entity.Person;
 import com.bank.BankSystem.Repository.BankCardRepository;
 import com.bank.BankSystem.Repository.PersonRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,14 +22,34 @@ public class BankSystemController {
     @Autowired
     private BankCardRepository bankCardRepository;
 
+    Logger logger = LoggerFactory.getLogger(BankSystemController.class);
+
     @PostMapping("/addPerson")
-    public String addPerson(@RequestBody Person person) {
-        this.personRepository.save(person);
-        return "Person successfully added!";
+    public ResponseEntity<String> addPerson(@RequestBody Person person) {
+        String error = "";
+        if(person.getName() == null) {
+            error = error + "name ";
+        } if(person.getSurname() == null) {
+            error = error + "surname ";
+        } if(person.getPersonalNumber() == null) {
+            error = error + "personalNumber";
+        }
+        if (error != "") {
+            return ResponseEntity.badRequest().body("Invalid input missing: " + error);
+        }
+        else {
+            this.personRepository.save(person);
+            logger.trace("TRACE message");
+            logger.info("INFO message");
+            return ResponseEntity.ok("Person successfully added!");
+        }
     }
 
     @GetMapping("/findAll")
-    public List<Person> findAll() {
-        return this.personRepository.findAll();
+    public ResponseEntity<List<Person>> findAll() {
+        if(this.personRepository.findAll().isEmpty()) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(this.personRepository.findAll());
+        }
+        return ResponseEntity.ok(this.personRepository.findAll());
     }
 }
