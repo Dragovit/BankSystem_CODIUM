cb111ee2e3af888520cfbc6a369785864e1be85b
Dragovit <palitomcik@gmail.com>
2023-06-29 21:07:40 +0200

    Little updates

--- BankSystem/src/main/java/com/bank/BankSystem/Controller/BankSystemController.java
+++ BankSystem/src/main/java/com/bank/BankSystem/Controller/BankSystemController.java
@@ -6,6 +6,7 @@ import com.bank.BankSystem.Repository.PersonRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,6 +25,7 @@ public class BankSystemController {
 
     Logger logger = LoggerFactory.getLogger(BankSystemController.class);
 
+
     @PostMapping("/addPerson")
     public ResponseEntity<String> addPerson(@RequestBody Person person) {
         String error = "";
@@ -34,24 +36,25 @@ public class BankSystemController {
             error = error + "surname ";
         }
         if (person.getPersonalNumber() == null) {
-            error = error + "personalNumber";
+            error = error + "personalNumber ";
         }
         if (!error.equals("")) {
             return ResponseEntity.badRequest().body("Invalid input missing: " + error);
         } else {
             this.personRepository.save(person);
-            logger.trace("Je zapnuty LOCAL profil");
+            logger.debug("Je zapnuty LOCAL profil");
             logger.info("Je zapnuty PRODUCTION profil");
             return ResponseEntity.ok("Person successfully added!");
         }
 
     }
-
     @GetMapping("/findAll")
     public ResponseEntity<List<Person>> findAll() {
         if(this.personRepository.findAll().isEmpty()) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(this.personRepository.findAll());
         }
+        logger.debug("Je zapnuty LOCAL profil");
+        logger.info("Je zapnuty PRODUCTION profil");
         return ResponseEntity.ok(this.personRepository.findAll());
     }
 
